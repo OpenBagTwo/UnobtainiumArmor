d9f321bbbd566a4ad80426bc3eb6f15d044ec04c
OpenBagTwo <openbagtwo@gmail.com>
2022-04-25 09:33:57 -0400

    Register the items!

--- src/main/java/net/openbagtwo/unobtainium_armor/UnobtainiumArmorMod.java
+++ src/main/java/net/openbagtwo/unobtainium_armor/UnobtainiumArmorMod.java
@@ -1,6 +1,7 @@
 package net.openbagtwo.unobtainium_armor;
 
 import net.fabricmc.api.ModInitializer;
+import net.openbagtwo.unobtainium_armor.items.RegisterItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +16,7 @@ public class UnobtainiumArmorMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		RegisterItems.register();
 		LOGGER.info("UnobtainiumArmor mod initialized.");
 	}
 }
