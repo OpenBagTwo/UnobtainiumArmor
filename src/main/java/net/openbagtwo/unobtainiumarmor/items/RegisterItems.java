e4dd8c429d8714045a1226ce64c82d577dc26a58
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2022-08-13 12:05:05 -0400

    Register sapphire everything

--- src/main/java/net/openbagtwo/unobtainiumarmor/items/RegisterItems.java
+++ src/main/java/net/openbagtwo/unobtainiumarmor/items/RegisterItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.openbagtwo.unobtainiumarmor.UnobtainiumArmorMod;
 import net.openbagtwo.unobtainiumarmor.materials.AlexandriteMaterial;
 import net.openbagtwo.unobtainiumarmor.materials.PlatinumMaterial;
+import net.openbagtwo.unobtainiumarmor.materials.SapphireMaterial;
 import net.openbagtwo.unobtainiumarmor.materials.UnobtainiumMaterial;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
@@ -13,12 +14,15 @@ import net.minecraft.util.registry.Registry;
 import nourl.mythicmetals.item.MythicItems;
 
 import static net.openbagtwo.unobtainiumarmor.items.Alexandrite.ALEXANDRITE;
+import static net.openbagtwo.unobtainiumarmor.items.Sapphire.SAPPHIRE;
 
 public class RegisterItems {
 
     public static final ArmorMaterial unobtainiumMaterial = new UnobtainiumMaterial();
     public static final ArmorMaterial platinumMaterial = new PlatinumMaterial();
     public static final ArmorMaterial alexandriteMaterial = new AlexandriteMaterial();
+
+    public static final ArmorMaterial sapphireMaterial = new SapphireMaterial();
 
     public static final Item UNOBTAINIUM_HELMET = new ArmorItem(unobtainiumMaterial, EquipmentSlot.HEAD, new Item.Settings().group(MythicItems.UNOBTAINIUM.getGroup()));
     public static final Item UNOBTAINIUM_CHESTPLATE = new ArmorItem(unobtainiumMaterial, EquipmentSlot.CHEST, new Item.Settings().group(MythicItems.UNOBTAINIUM.getGroup()));
@@ -35,6 +39,11 @@ public class RegisterItems {
     public static final Item ALEXANDRITE_LEGGINGS = new ArmorItem(alexandriteMaterial, EquipmentSlot.LEGS, new Item.Settings().group(ALEXANDRITE.getGroup()));
     public static final Item ALEXANDRITE_BOOTS = new ArmorItem(alexandriteMaterial, EquipmentSlot.FEET, new Item.Settings().group(ALEXANDRITE.getGroup()));
 
+    public static final Item SAPPHIRE_HELMET = new ArmorItem(sapphireMaterial, EquipmentSlot.HEAD, new Item.Settings().group(SAPPHIRE.getGroup()));
+    public static final Item SAPPHIRE_CHESTPLATE = new ArmorItem(sapphireMaterial, EquipmentSlot.CHEST, new Item.Settings().group(SAPPHIRE.getGroup()));
+    public static final Item SAPPHIRE_LEGGINGS = new ArmorItem(sapphireMaterial, EquipmentSlot.LEGS, new Item.Settings().group(SAPPHIRE.getGroup()));
+    public static final Item SAPPHIRE_BOOTS = new ArmorItem(sapphireMaterial, EquipmentSlot.FEET, new Item.Settings().group(SAPPHIRE.getGroup()));
+
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "unobtainium_helmet"), UNOBTAINIUM_HELMET);
         Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "unobtainium_chestplate"), UNOBTAINIUM_CHESTPLATE);
@@ -49,5 +58,9 @@ public class RegisterItems {
         Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "alexandrite_chestplate"), ALEXANDRITE_CHESTPLATE);
         Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "alexandrite_leggings"), ALEXANDRITE_LEGGINGS);
         Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "alexandrite_boots"), ALEXANDRITE_BOOTS);
+        Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "sapphire_helmet"), SAPPHIRE_HELMET);
+        Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "sapphire_chestplate"), SAPPHIRE_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "sapphire_leggings"), SAPPHIRE_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier("unobtainiumarmor", "sapphire_boots"), SAPPHIRE_BOOTS);
     }
 }
