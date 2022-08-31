df8f03c3da3e495c3e5fd6ef13f81e4e36af5bcf
OpenBagTwo <openbagtwo@gmail.com>
2022-08-31 00:30:39 -0400

    Infinity armor
    Model needs some fixing, but it kinda looks cool this way imo

--- src/main/java/net/openbagtwo/unobtainiumarmor/UnobtainiumArmorModClient.java
+++ src/main/java/net/openbagtwo/unobtainiumarmor/UnobtainiumArmorModClient.java
@@ -7,6 +7,7 @@ import net.minecraft.client.particle.SpitParticle;
 import net.minecraft.item.Item;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
+import net.openbagtwo.unobtainiumarmor.entity.client.armor.MythrilArmorModel;
 import net.openbagtwo.unobtainiumarmor.entity.client.armor.MythrilArmorRenderer;
 import net.openbagtwo.unobtainiumarmor.factory.ArmorItemFactory;
 import net.openbagtwo.unobtainiumarmor.items.Armors;
@@ -18,40 +19,48 @@ import java.util.Map;
 
 public class UnobtainiumArmorModClient implements ClientModInitializer {
 
-    public static Item[] getArmorItems(Map<String, Item> group){
-        ArrayList<Item> armorItems = new ArrayList<>();
-        for (Map.Entry<String, Item> item : group.entrySet()){
-            String[] parsed = item.getKey().split("_");
-            if (ArmorItemFactory.SLOT_MAP.containsValue(parsed[parsed.length - 1])){
-                armorItems.add(item.getValue());
-            }
-        }
-        return armorItems.toArray(new Item[0]);
+  public static Item[] getArmorItems(Map<String, Item> group) {
+    ArrayList<Item> armorItems = new ArrayList<>();
+    for (Map.Entry<String, Item> item : group.entrySet()) {
+      String[] parsed = item.getKey().split("_");
+      if (ArmorItemFactory.SLOT_MAP.containsValue(parsed[parsed.length - 1])) {
+        armorItems.add(item.getValue());
+      }
     }
+    return armorItems.toArray(new Item[0]);
+  }
 
-    @Override
-    public void onInitializeClient() {
-        ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) -> {
-            registry.register(new Identifier(UnobtainiumArmorMod.MOD_ID, "particle/f_ray"));
+  @Override
+  public void onInitializeClient() {
+    ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE)
+        .register(((atlasTexture, registry) -> {
+          registry.register(new Identifier(UnobtainiumArmorMod.MOD_ID, "particle/f_ray"));
         }));
 
-        ParticleFactoryRegistry.getInstance().register(RegisterParticles.F_RAY, SpitParticle.Factory::new);
+    ParticleFactoryRegistry.getInstance()
+        .register(RegisterParticles.F_RAY, SpitParticle.Factory::new);
 
-        GeoArmorRenderer.registerArmorRenderer(
-                MythrilArmorRenderer.MYTHRIL,
-                getArmorItems(Armors.MYTHRIL)
-        );
-        GeoArmorRenderer.registerArmorRenderer(
-                MythrilArmorRenderer.createMythrilRenderer("cythril"),
-                getArmorItems(Armors.CYTHRIL)
-        );
-        GeoArmorRenderer.registerArmorRenderer(
-                MythrilArmorRenderer.createMythrilRenderer("topaz"),
-                getArmorItems(Armors.TOPAZ)
-        );
-        GeoArmorRenderer.registerArmorRenderer(
-                MythrilArmorRenderer.createMythrilRenderer("sapphire"),
-                getArmorItems(Armors.SAPPHIRE)
-        );
-    }
+    GeoArmorRenderer.registerArmorRenderer(
+        MythrilArmorRenderer.MYTHRIL,
+        getArmorItems(Armors.MYTHRIL)
+    );
+    GeoArmorRenderer.registerArmorRenderer(
+        MythrilArmorRenderer.createMythrilRenderer("cythril"),
+        getArmorItems(Armors.CYTHRIL)
+    );
+    GeoArmorRenderer.registerArmorRenderer(
+        MythrilArmorRenderer.createMythrilRenderer("topaz"),
+        getArmorItems(Armors.TOPAZ)
+    );
+    GeoArmorRenderer.registerArmorRenderer(
+        MythrilArmorRenderer.createMythrilRenderer("sapphire"),
+        getArmorItems(Armors.SAPPHIRE)
+    );
+    GeoArmorRenderer.registerArmorRenderer(
+        MythrilArmorRenderer.createMythrilRenderer(
+            new MythrilArmorModel().setModel("infinity_armor").setTexture("infinity_armor_texture")
+        ),
+        getArmorItems(Armors.INFINITY)
+    );
+  }
 }
--- src/main/java/net/openbagtwo/unobtainiumarmor/items/Armors.java
+++ src/main/java/net/openbagtwo/unobtainiumarmor/items/Armors.java
@@ -21,115 +21,122 @@ import static net.openbagtwo.unobtainiumarmor.factory.MaterialFactory.slotValues
 
 public class Armors {
 
-    public static final Map<String, Item> UNOBTAINIUM = ArmorItemFactory.makeArmorSet(
-            MaterialFactory.makeMaterial("unobtainium", MythicItems.UNOBTAINIUM
-            ).setDurability(durabilityValues(1)
-            ).setProtection(slotValues(100, 200, 150, 100)
-            ).setEnchantability(15).setToughness(100).setEquipSound(
-                    SoundEvents.ENTITY_WITHER_SPAWN
-            )
-    ).getItemsForRegistration();
+  public static final Map<String, Item> UNOBTAINIUM = ArmorItemFactory.makeArmorSet(
+      MaterialFactory.makeMaterial("unobtainium", MythicItems.UNOBTAINIUM
+      ).setDurability(durabilityValues(1)
+      ).setProtection(slotValues(100, 200, 150, 100)
+      ).setEnchantability(15).setToughness(100).setEquipSound(
+          SoundEvents.ENTITY_WITHER_SPAWN
+      )
+  ).getItemsForRegistration();
 
-    public static final Map<String, Item> ALEXANDRITE = ArmorItemFactory.makeArmorSet("alexandrite"
-            ).setDurability(durabilityValues(16)
-            ).setProtection(slotValues(1, 2, 2, 1)
-            ).setEnchantability(36).setEquipSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP
-    ).getItemsForRegistration();
+  public static final Map<String, Item> ALEXANDRITE = ArmorItemFactory.makeArmorSet("alexandrite"
+  ).setDurability(durabilityValues(16)
+  ).setProtection(slotValues(1, 2, 2, 1)
+  ).setEnchantability(36).setEquipSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP
+  ).getItemsForRegistration();
 
-    public static final Map<String, Item> CATS_EYE = ArmorItemFactory.makeArmorSet("cats_eye_gem"
-            ).setDurability(slotValues(4, 3, 2, 1, 9)
-            ).setProtection(slotValues(1, 1, 1, 1)
-            ).setEnchantability(2).setEquipSound(SoundEvents.ENTITY_CAT_PURREOW
-    ).setElytraProperties(0.999, false
-    ).getItemsForRegistration();
+  public static final Map<String, Item> CATS_EYE = ArmorItemFactory.makeArmorSet("cats_eye_gem"
+  ).setDurability(slotValues(4, 3, 2, 1, 9)
+  ).setProtection(slotValues(1, 1, 1, 1)
+  ).setEnchantability(2).setEquipSound(SoundEvents.ENTITY_CAT_PURREOW
+  ).setElytraProperties(0.999, false
+  ).getItemsForRegistration();
 
-    public static final Map<String, Item> MYTHRIL = ArmorItemFactory.makeArmorSet(
-            "mythril",
-            MaterialFactory.makeMaterial("slime", Items.SLIME_BALL)
-                    .setDurability(durabilityValues(14))
-                    .setProtection(slotValues(3, 8, 6, 3))
-                    .setEnchantability(1)
-                    .setEquipSound(SoundEvents.ENTITY_SLIME_ATTACK)
-                    .setKnockbackResistance(0.99F)
-    ).setElytraProperties(0.8, false).makeMythril().getItemsForRegistration();
+  public static final Map<String, Item> MYTHRIL = ArmorItemFactory.makeArmorSet(
+      "mythril",
+      MaterialFactory.makeMaterial("slime", Items.SLIME_BALL)
+          .setDurability(durabilityValues(14))
+          .setProtection(slotValues(3, 8, 6, 3))
+          .setEnchantability(1)
+          .setEquipSound(SoundEvents.ENTITY_SLIME_ATTACK)
+          .setKnockbackResistance(0.99F)
+  ).setElytraProperties(0.8, false).makeMythril().getItemsForRegistration();
 
-    public static final Map<String, Item> CITRINE = Collections.singletonMap(
-            "citrine_crystal",
-            new Item(new FabricItemSettings().group(ItemGroup.MATERIALS))
-    );
-    public static final Map<String, Item> CYTHRIL = ArmorItemFactory.makeArmorSet(
-            MaterialFactory.makeMaterial("citrine", CITRINE.values().iterator().next())
-    ).setDurability(durabilityValues(18)
-    ).setProtection(slotValues(1, 2, 2, 1)
-    ).setEnchantability(28
-    ).setEquipSound(SoundEvents.BLOCK_BEACON_POWER_SELECT
-    ).setElytraProperties(0.97, false
-    ).makeMythril().getItemsForRegistration();
+  public static final Map<String, Item> CITRINE = Collections.singletonMap(
+      "citrine_crystal",
+      new Item(new FabricItemSettings().group(ItemGroup.MATERIALS))
+  );
+  public static final Map<String, Item> CYTHRIL = ArmorItemFactory.makeArmorSet(
+      MaterialFactory.makeMaterial("citrine", CITRINE.values().iterator().next())
+  ).setDurability(durabilityValues(18)
+  ).setProtection(slotValues(1, 2, 2, 1)
+  ).setEnchantability(28
+  ).setEquipSound(SoundEvents.BLOCK_BEACON_POWER_SELECT
+  ).setElytraProperties(0.97, false
+  ).makeMythril().getItemsForRegistration();
 
-    public static final Map<String, Item> PLATINUM = ArmorItemFactory.makeArmorSet(
-            MaterialFactory.makeMaterial("platinum", MythicItems.PLATINUM_INGOT
-            ).setDurability(durabilityValues(9)
-            ).setProtection(slotValues(3, 6, 4, 2)
-            ).setEnchantability(24
-            ).setEquipSound(SoundEvents.ITEM_ARMOR_EQUIP_GOLD)
-    ).getItemsForRegistration();
+  public static final Map<String, Item> PLATINUM = ArmorItemFactory.makeArmorSet(
+      MaterialFactory.makeMaterial("platinum", MythicItems.PLATINUM_INGOT
+      ).setDurability(durabilityValues(9)
+      ).setProtection(slotValues(3, 6, 4, 2)
+      ).setEnchantability(24
+      ).setEquipSound(SoundEvents.ITEM_ARMOR_EQUIP_GOLD)
+  ).getItemsForRegistration();
 
-    public static final Map<String, Item> SAPPHIRE = ArmorItemFactory.makeArmorSet("sapphire"
-    ).setDurability(slotValues(1, 1, 1, 1, 16)
-    ).setProtection(slotValues(1, 1, 1, 1)
-    ).setEnchantability(64).setEquipSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO
-    ).makeMythril().getItemsForRegistration();
+  public static final Map<String, Item> SAPPHIRE = ArmorItemFactory.makeArmorSet("sapphire"
+  ).setDurability(slotValues(1, 1, 1, 1, 16)
+  ).setProtection(slotValues(1, 1, 1, 1)
+  ).setEnchantability(64).setEquipSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO
+  ).makeMythril().getItemsForRegistration();
 
-    // Adapting from MoreGems' Gilded Carbonado
-    public static final Map<String, Item> BUMBLENADO = ArmorItemFactory.makeArmorSet("bumblenado"
-    ).setDurability(durabilityValues(33)
-    ).setProtection(slotValues(3, 8, 6, 3)
-    ).setEnchantability(10
-    ).setEquipSound(SoundEvents.ENTITY_BEE_LOOP_AGGRESSIVE
-    ).setToughness(0.75F
-    ).setKnockbackResistance(0.25F
-    ).setElytraProperties(0.87, true
-    ).getItemsForRegistration();
+  // Adapting from MoreGems' Gilded Carbonado
+  public static final Map<String, Item> BUMBLENADO = ArmorItemFactory.makeArmorSet("bumblenado"
+  ).setDurability(durabilityValues(33)
+  ).setProtection(slotValues(3, 8, 6, 3)
+  ).setEnchantability(10
+  ).setEquipSound(SoundEvents.ENTITY_BEE_LOOP_AGGRESSIVE
+  ).setToughness(0.75F
+  ).setKnockbackResistance(0.25F
+  ).setElytraProperties(0.87, true
+  ).getItemsForRegistration();
 
-    public static final Map<String, Item> MUSHIE_COIN_COIN = Collections.singletonMap(
-            "goomba_coin_coin",
-            MushieLayer.GOOMBA_COIN_COIN
-    );
-    public static final Map<String, Item> MUSHIE = ArmorItemFactory.makeArmorSet(
-            "goomba", MushieLayer.GOOMBA_LAYER
-    ).setElytraProperties(0.93, false
-    ).getItemsForRegistration();
+  public static final Map<String, Item> MUSHIE_COIN_COIN = Collections.singletonMap(
+      "goomba_coin_coin",
+      MushieLayer.GOOMBA_COIN_COIN
+  );
+  public static final Map<String, Item> MUSHIE = ArmorItemFactory.makeArmorSet(
+      "goomba", MushieLayer.GOOMBA_LAYER
+  ).setElytraProperties(0.93, false
+  ).getItemsForRegistration();
 
-    public static final Map<String, Item> TOPAZ = ArmorItemFactory.makeArmorSet("topaz"
-    ).setEquipSound(SoundEvents.BLOCK_TUFF_STEP
-    ).setElytraProperties(0.95, false
-    ).makeMythril(
-    ).getItemsForRegistration();
+  public static final Map<String, Item> TOPAZ = ArmorItemFactory.makeArmorSet("topaz"
+  ).setEquipSound(SoundEvents.BLOCK_TUFF_STEP
+  ).setElytraProperties(0.95, false
+  ).makeMythril(
+  ).getItemsForRegistration();
 
-    public static Map<String, Item> allItems(){
-        HashMap<String, Item> allItems = new HashMap<>();
-        allItems.putAll(UNOBTAINIUM);
-        allItems.putAll(ALEXANDRITE);
-        allItems.putAll(CATS_EYE);
-        allItems.putAll(MYTHRIL);
-        allItems.putAll(CITRINE);
-        allItems.putAll(CYTHRIL);
-        allItems.putAll(PLATINUM);
-        allItems.putAll(SAPPHIRE);
-        allItems.putAll(BUMBLENADO);
-        allItems.putAll(MUSHIE_COIN_COIN);
-        allItems.putAll(MUSHIE);
-        allItems.putAll(TOPAZ);
-        return allItems;
+  public static final Map<String, Item> INFINITY = ArmorItemFactory.makeArmorSet(
+      MaterialFactory.makeMaterial("infinity", Items.GHAST_TEAR)
+  ).setEquipSound(SoundEvents.UI_TOAST_CHALLENGE_COMPLETE
+  ).makeMythril(
+  ).getItemsForRegistration();
+
+  public static Map<String, Item> allItems() {
+    HashMap<String, Item> allItems = new HashMap<>();
+    allItems.putAll(UNOBTAINIUM);
+    allItems.putAll(ALEXANDRITE);
+    allItems.putAll(CATS_EYE);
+    allItems.putAll(MYTHRIL);
+    allItems.putAll(CITRINE);
+    allItems.putAll(CYTHRIL);
+    allItems.putAll(PLATINUM);
+    allItems.putAll(SAPPHIRE);
+    allItems.putAll(BUMBLENADO);
+    allItems.putAll(MUSHIE_COIN_COIN);
+    allItems.putAll(MUSHIE);
+    allItems.putAll(TOPAZ);
+    allItems.putAll(INFINITY);
+    return allItems;
+  }
+
+  public static void register() {
+    for (Map.Entry<String, Item> item : allItems().entrySet()) {
+      Registry.register(
+          Registry.ITEM,
+          new Identifier(UnobtainiumArmorMod.MOD_ID, item.getKey()),
+          item.getValue()
+      );
     }
-
-    public static void register() {
-        for (Map.Entry<String, Item> item : allItems().entrySet()){
-            Registry.register(
-                    Registry.ITEM,
-                    new Identifier(UnobtainiumArmorMod.MOD_ID, item.getKey()),
-                    item.getValue()
-            );
-        }
-    }
+  }
 }
